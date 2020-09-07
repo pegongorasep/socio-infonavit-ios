e26e0f73bbcd1db21cace8fdc03db46537ee8d9e
Pedro Antonio Góngora <pedro.gongora@dacodes.com.mx>
2020-09-07 13:29:48 -0500

    initial project

---
+++ socio-infonavit-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  socio-infonavit-ios
+//
+//  Created by Pedro Antonio Góngora Sepúlveda on 07/09/20.
+//  Copyright © 2020 Nextia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
